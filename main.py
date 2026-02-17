fe14240dcd811af5a521b2c30ae68fd81a260f04
Denton <163344926+cDenton1@users.noreply.github.com>
2026-02-17 15:21:47 -0700

    Refactor run function by removing commented code

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ def run():
     from Honeypot.wifi_ap import startAP
     from Honeypot.config import SSID, SECURITY
     from Honeypot.portal import startServer
-#     from Honeypot.connections import monitorConnect
-#     from Honeypot.dns import startDNS
     from Honeypot.background import backgroundTask
     import time
     import _thread
@@ -13,7 +11,6 @@ def run():
         f.write("")
     
     ap = startAP(SSID, SECURITY)     # call for the ap to start
-#     _thread.start_new_thread(startDNS, ())
-#     _thread.start_new_thread(monitorConnect, (ap,))
     _thread.start_new_thread(backgroundTask, (ap,))
+
     startServer()
